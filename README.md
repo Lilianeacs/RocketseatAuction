89ac5a11dbaa3db4425d8d2c3a890c5e28f224e4
Liliane Souza <liliane.a.cruz.souza@gmail.com>
2024-02-06 23:58:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RocketseatAuction
+ Projeto do intensivo da Rocketseat de C# - API de Leilão
---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Offers> Offers { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source= D:\\Rocketseat-CSharp\\leilaoDbNLW.db");
+    }
+}
